a2ba235f4bf8b2969f95c76524e2333308652c00
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2018-04-02 21:31:24 +0300

    tests: add main.TestGetInputText

--- main_test.go
+++ main_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"testing"
+)
 
 func TestParseURLs(t *testing.T) {
 	tests := []struct {
@@ -13,7 +18,7 @@ func TestParseURLs(t *testing.T) {
 	}
 	urls, err := parseURLs("test urls: https://domain1.com http://domain2.com ftp://domain3.com")
 	if err != nil {
-		t.Errorf("could not parse urls: %v\n", err)
+		t.Errorf("could not parse urls: %v", err)
 	}
 	for _, test := range tests {
 		found := false
@@ -30,5 +35,21 @@ func TestParseURLs(t *testing.T) {
 		if found != test.shouldBeFound {
 			t.Errorf("url %s should %s be found", test.url, shouldBeFoundStr)
 		}
+	}
+}
+
+func TestGetInputText(t *testing.T) {
+	expected := "test urls list"
+	rr := []io.ReadCloser{
+		ioutil.NopCloser(
+			bytes.NewBufferString(expected),
+		),
+	}
+	inputText, err := getInputText(rr)
+	if err != nil {
+		t.Errorf("could not get input text: %v", err)
+	}
+	if expected != inputText {
+		t.Errorf("expected inputText \"%s\" but got \"%s\"", expected, inputText)
 	}
 }
