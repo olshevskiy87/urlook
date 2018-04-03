8af0a41bec000b4730a3099bcb729420eab30cef
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2018-04-03 22:56:38 +0300

    tests: add main.TestGetReaders

--- main_test.go
+++ main_test.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
+	"os"
 	"testing"
+
+	"github.com/alexflint/go-arg"
 )
 
 func TestParseURLs(t *testing.T) {
@@ -52,4 +55,110 @@ func TestGetInputText(t *testing.T) {
 	if expected != inputText {
 		t.Errorf("expected inputText \"%s\" but got \"%s\"", expected, inputText)
 	}
+}
+
+func TestGetReaders(t *testing.T) {
+	oldArgs := os.Args
+	defer func() {
+		os.Args = oldArgs
+	}()
+	tmpFile, err := ioutil.TempFile(os.TempDir(), "urlook_test")
+	if err != nil {
+		t.Fatalf("could not make temp file: %v", err)
+	}
+	defer func(fname string) {
+		err := os.Remove(fname)
+		if err != nil {
+			t.Logf("could not close temp file %s: %v", fname, err)
+		}
+	}(tmpFile.Name())
+	t.Run(
+		"one input file only",
+		func(t *testing.T) {
+			os.Args = []string{os.Args[0], tmpFile.Name()}
+			var args argsType
+			arg.MustParse(&args)
+
+			readers, err := getReaders(args)
+			if err != nil {
+				t.Fatalf("could not get readers: %v", err)
+			}
+			defer closeReaders(readers)
+
+			readersCnt := len(readers)
+			if readersCnt != 1 {
+				t.Fatalf("expected readers count 1 but got %d", readersCnt)
+			}
+		},
+	)
+	t.Run(
+		"stdin only",
+		func(t *testing.T) {
+			os.Args = []string{os.Args[0]}
+			stdin := os.Stdin
+			defer func() {
+				os.Stdin = stdin
+			}()
+			r, w, err := os.Pipe()
+			if err != nil {
+				t.Fatalf("could not open pipe: %v", err)
+			}
+			os.Stdin = r
+			w.WriteString("test input string\n")
+			defer w.Close()
+
+			var args argsType
+			arg.MustParse(&args)
+
+			readers, err := getReaders(args)
+			if err != nil {
+				t.Fatalf("could not get readers: %v", err)
+			}
+			defer closeReaders(readers)
+
+			readersCnt := len(readers)
+			if readersCnt != 1 {
+				t.Fatalf("expected readers count 1 but got %d", readersCnt)
+			}
+		},
+	)
+	t.Run(
+		"stdin and one input file",
+		func(t *testing.T) {
+			os.Args = []string{os.Args[0], "someFile.md"}
+			stdin := os.Stdin
+			defer func() {
+				os.Stdin = stdin
+			}()
+			r, w, err := os.Pipe()
+			if err != nil {
+				t.Fatalf("could not open pipe: %v", err)
+			}
+			os.Stdin = r
+			w.WriteString("test input string\n")
+			defer w.Close()
+
+			var args argsType
+			arg.MustParse(&args)
+
+			_, err = getReaders(args)
+			if err == nil {
+				t.Fatalf("expected getReaders returns error but got nil")
+			}
+		},
+	)
+	t.Run(
+		"neither stdin nor input file",
+		func(t *testing.T) {
+			os.Args = []string{os.Args[0]}
+
+			var args argsType
+			arg.MustParse(&args)
+
+			_, err = getReaders(args)
+			if err == nil {
+				t.Fatalf("expected getReaders returns error but got nil")
+			}
+		},
+	)
 }
