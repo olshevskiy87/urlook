fcda8659e5351435132aafd82601e55acdb6e990
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2018-04-04 23:30:25 +0300

    tests: add status.TestIsStatus

--- urlook/status/status_test.go
+++ urlook/status/status_test.go
@@ -31,3 +31,37 @@ func TestString(t *testing.T) {
 		)
 	}
 }
+
+func TestIsStatus(t *testing.T) {
+	s := New(0)
+
+	type CodeTest []struct {
+		Code    int
+		IsValid bool
+	}
+	tests := []struct {
+		Name      string
+		Func      func() bool
+		CodeTests CodeTest
+	}{
+		{"IsInfo", s.IsInfo, CodeTest{{100, true}, {200, false}}},
+		{"IsSuccess", s.IsSuccess, CodeTest{{200, true}, {300, false}}},
+		{"IsRedirect", s.IsRedirect, CodeTest{{300, true}, {400, false}}},
+		{"IsClientError", s.IsClientError, CodeTest{{400, true}, {500, false}}},
+		{"IsServerError", s.IsServerError, CodeTest{{500, true}, {600, false}}},
+	}
+	for _, test := range tests {
+		for _, cT := range test.CodeTests {
+			t.Run(
+				fmt.Sprintf("%s: code %d", test.Name, cT.Code),
+				func(t *testing.T) {
+					s.Code = cT.Code
+					isValid := test.Func()
+					if isValid != cT.IsValid {
+						t.Fatalf("expected status %s %v but got %v\n", test.Name, cT.IsValid, isValid)
+					}
+				},
+			)
+		}
+	}
+}
